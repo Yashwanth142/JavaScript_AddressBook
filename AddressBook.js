1676a92c09b2dab4b55de5478689090d7d5f60d8
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-02-16 08:18:58 +0530

    putting validations to the contacts

--- AddressBook.js
+++ AddressBook.js
@@ -23,7 +23,7 @@ class Person
         this.phoneNumber = parameters[6]; 
         this.emailId = parameters[7];
     }
-    //methid to return everything in string
+    //method to return everything in string
     toString()
     {
         return `First Name: ${this.firstName}\nLast Name: ${this.lastName}\nAddress: ${this.address}\nCity: ${this.city}\nState: ${this.state}\nZipCode: ${this.zipCode}\nPhone Number: ${this.phoneNumber}\nEmail-Id: ${this.emailId}`;
@@ -31,18 +31,48 @@ class Person
 }
 try
 {
+    //regex patterns
+    let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+    let addresspattern = new RegExp('^[A-Za-z\\s]{4,}$');
+    let emailPattern = new RegExp('^[a-z]([+.-_]{1}\w+)?@[a-z0-9]+.[a-z]{2,3}(.[a-z]{2,3})?$');
+    let phoneNumPattern =new RegExp('^[0-9]\\s[6-9]{10}$');
+    let zipCodePattern = new RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
+    //FirstName
     let firstName = prompt('Enter FirstName: ');
+    if(!namePattern.test(firstName))
+        throw 'Error!! First name should start with Capital and Minimum 3 Characters';
+    //LastName
     let lastName = prompt('Enter lastName: ');
+    if(!namePattern.test(lastName))
+        throw 'Error!! Last name should start with Capital and Minimum 3 Characters';
+    //Address
     let address = prompt('Enter Address: ');
+    if(!addresspattern.test(address))
+        throw 'Error!! Address should have minimum 4 characters';
+    //city
     let city = prompt('Enter city: ');
+    if(!addresspattern.test(city))
+        throw 'Error!! City should have minimum 4 characters';
+    //state
     let state = prompt('Enter state: ');
-    let zipCode = parseInt(prompt('Enter Zip Code: '));
-    let phoneNumber = parseInt(prompt('Enter Phone Number: '));
+    if(!addresspattern.test(state))
+        throw 'Error!! State should have minimum 4 characters';
+    //zipCode
+    let zipCode = prompt('Enter Zip Code: ');
+    if(!zipCodePattern.test(zipCode))
+        throw 'Error!! ZipCode is not valid';
+    //phone number
+    let phoneNumber = prompt('Enter Phone Number: ');
+    if(phoneNumPattern.test(phoneNumber))
+        throw 'Error!! PhoneNumber is not valid';
+    //email id
     let emailId = prompt('Enter Email Id: ');
-    
+    if(emailPattern.test(emailId))
+        throw 'Error!! Email Id is not valid';
+    //Object for class
     let person = new Person(firstName,lastName,address,city,state,zipCode,phoneNumber,emailId);
-    console.log("\n************************Contact Deatils******************");
-   console.log(person.toString());
+    console.log("\n************************Contacts Deatils******************");
+    console.log(person.toString());
 }
 catch(e)
 {
